25645441c8fb61ec4b21c0e8abf37669ca4b2e1f
Neil Coutinho <ncoutin@thoughtworks.com>
2017-02-17 00:14:29 +0200

    [neil] add capability to add drone secret for remote provisioning server rsa key

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -23,8 +23,11 @@ function start_drone_ci(){
 function setup_drone_ci_github(){
   source env.sh
   drone secret add --image=themast/docker-wp-theme-build:latest \
-  the-mast/the-mast-theme \
-  GITHUB_TOKEN $GITHUB_TOKEN
+    the-mast/the-mast-theme \
+    GITHUB_TOKEN $GITHUB_TOKEN
+  drone secret add --image=themast/docker-wp-theme-build:latest \
+    the-mast/the-mast-theme \
+    REMOTE_RSA_KEY $REMOTE_RSA_KEY
 }
 
 function main(){
