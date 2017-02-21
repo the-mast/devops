0a5e910e8ac9ab91ad2515f2513bbb497f329826
Neil Coutinho <ncoutin@thoughtworks.com>
2017-02-21 14:24:38 +0200

    [felix, neil] update ci provision for new theme repo

--- ci/add_drone_secrets.sh
+++ ci/add_drone_secrets.sh
@@ -9,23 +9,13 @@ function setup_drone_ci(){
   source env.sh
 
   drone secret add --conceal --image=themast/docker-wp-theme-build:latest \
-    the-mast/the-mast-theme \
+    the-mast/presson-theme \
     GITHUB_TOKEN $GITHUB_TOKEN
   drone secret add --conceal --image=themast/docker-wp-theme-build:latest \
-    the-mast/the-mast-theme \
+    the-mast/presson-theme \
     REMOTE_IP_ADDRESS $IP_ADDRESS
   drone secret add --conceal --image=themast/docker-wp-theme-build:latest \
-    the-mast/the-mast-theme \
-    REMOTE_RSA_KEY "$REMOTE_RSA_KEY"
-
-  drone secret add --conceal --image=themast/docker-wp-theme-build:latest \
-    the-mast/webstarterkit-spike \
-    GITHUB_TOKEN $GITHUB_TOKEN
-  drone secret add --conceal --image=themast/docker-wp-theme-build:latest \
-    the-mast/webstarterkit-spike \
-    REMOTE_IP_ADDRESS $IP_ADDRESS
-  drone secret add --conceal --image=themast/docker-wp-theme-build:latest \
-    the-mast/webstarterkit-spike \
+    the-mast/presson-theme \
     REMOTE_RSA_KEY "$REMOTE_RSA_KEY"
 }
 
