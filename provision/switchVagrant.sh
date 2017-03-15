7380fb6c3a149c09913f24ac15cfd2f0c30eff1d
Brett Strydom <bstrydo@thoughtworks.com>
2017-03-15 12:36:15 +0200

    added some checks

--- provision/switchVagrant.sh
+++ provision/switchVagrant.sh
@@ -1,4 +1,34 @@
 #!/bin/sh
 
 . env.sh
-ansible-playbook -v switch.yml -i vagrantHosts.ini
+ansible-playbook -v playbooks/putDBsToReadonly.yml -i vagrantHosts.ini
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v playbooks/backup_wordpress.yml -i vagrantHosts.ini
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v playbooks/clearBox.yml -i vagrantHosts.ini -l production
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v site.yml -i vagrantHosts.ini -l production
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v playbooks/restoreUploads.yml -i vagrantHosts.ini -l production
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v playbooks/enableDBWrites.yml -i vagrantHosts.ini
