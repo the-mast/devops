8ccd5b644ef302853c31ad39963a0ce624885307
Brett Strydom <bstrydo@thoughtworks.com>
2017-03-15 12:36:31 +0200

    added some checks

--- provision/switch.sh
+++ provision/switch.sh
@@ -1,4 +1,44 @@
 #!/bin/sh
 
 . env.sh
-ansible-playbook -v switch.yml -i hosts.ini
+ansible-playbook -v playbooks/putDBsToReadonly.yml -i hosts.ini
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v playbooks/backup_wordpress.yml -i hosts.ini
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v playbooks/clearBox.yml -i hosts.ini -l production
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v site.yml -i hosts.ini -l production
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v playbooks/restoreUploads.yml -i hosts.ini -l production
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v playbooks/enableDBWrites.yml -i hosts.ini
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
+
+ansible-playbook -v switch.yml -i hosts.ini -l production
+if [ $? != 0 ]; then
+    echo "exiting"
+    exit 1
+fi
