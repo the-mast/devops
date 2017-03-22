a1be799c355bf41091ac7f36c68e0a236e40a294
Brett Strydom <bstrydo@thoughtworks.com>
2017-03-22 11:30:38 +0200

    added

---
+++ provision/news_day_africa.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export CONFIG_NAME='news_day_africa'
+export PRODUCTION_MAILSERVER_PASS='Ktw3Cmc9Gzxc'
+export PRODUCTION_WP_DB_PASS='eF4QCpf9K6N8r2Cy'
+export PRODUCTION_WP_DB_HOST='localhost'
+
+export STAGING_MAILSERVER_PASS='Ktw3Cmc9Gzxc'
+export STAGING_WP_DB_PASS='Qf2GbZxKqEHy3459'
+export STAGING_WP_DB_HOST='localhost'
+
+export WP_ADMIN_NAME='mast-admin'
+export WP_ADMIN_EMAIL='the-mast@thoughtworks.com'
+export WP_ADMIN_PASSWORD='Zfq4jV7MiTfB99L8'
+
+export AWS_ACCESS_KEY="AKIAINGWGXFAEWARXXEA"
+export AWS_SECRET_KEY="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"
