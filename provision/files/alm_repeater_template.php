c333671e424a4ea1496aa7f37494f2e6aab6e035
Neil Coutinho <ncoutin@thoughtworks.com>
2017-04-03 09:33:41 +0200

    modify repeater template

--- provision/files/alm_repeater_template.php
+++ provision/files/alm_repeater_template.php
@@ -1,9 +1,16 @@
+<?php if(!empty (get_the_post_thumbnail())): ?>
 <div class="related-story">
-    <div class="related-story-title">
-        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-        <div class="related-story-excerpt"><?php
-            $sentence = preg_split( '/(\.|!|\?)\s/', strip_tags( get_the_content()), 2, PREG_SPLIT_DELIM_CAPTURE );
-            echo $sentence[0]; ?>
+    <div class="related-story-title title">
+        <?php	foreach ($categories as $cat):
+            if( !in_array($cat->term_id, $excludes)) :
+                $category_link = get_category_link( $cat->term_id );
+                echo '<a href="' . esc_html($category_link) . '"><span>' . esc_html( $cat->name ) . '</span> </a>';
+                echo ' ';
+            endif;
+        endforeach ?>
+        <h3 class="archive-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <div class="related-story-excerpt">
+            <?php	echo $sentence[0]; ?>
         </div>
     </div>
     <div class="related-story-image">
@@ -13,3 +20,25 @@
         <?php the_time('jS F Y') ?>
     </div>
 </div>
+<?php
+else: ?>
+<div class="related-story">
+    <div class="full-length-title title">
+        <?php
+        foreach ($categories as $cat):
+        if( !in_array($cat->term_id, $excludes)) :
+        $category_link = get_category_link( $cat->term_id );
+        echo '<a href="' . esc_html($category_link) . '"><span>' . esc_html( $cat->name ) . '</span> </a>';
+        echo ' ';
+        endif;
+        endforeach ?>
+        <h3 class="archive-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <div class="related-story-excerpt">
+            <?php	echo $sentence[0]; ?>
+        </div>
+        <div class="related-story-date">
+            <?php the_time('jS F Y') ?>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
